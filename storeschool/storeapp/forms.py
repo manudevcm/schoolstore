194283497482d8f598c306f4e181dc20d54d2089
manudevcm <manudevcm@gmail.com>
2023-06-22 12:18:41 +0530

    added

---
+++ storeschool/storeapp/forms.py
@@ -0,0 +1,36 @@
+from .models import Department, Course, Details
+from django import forms
+
+
+
+
+class RegForm(forms.ModelForm):
+    MATERIAL_CHOICES = (
+        ('1', 'Note Book'),
+        ('2', 'Pen'),
+        ('3', 'Exam Papers'),
+        ('4', 'Eraser'),
+        ('5', 'Pencil'),
+        ('6', 'Sharpener'),
+    )
+    material_choices = forms.MultipleChoiceField(choices=MATERIAL_CHOICES,
+                                         widget=forms.CheckboxSelectMultiple)
+
+
+    class Meta:
+        model = Details
+        fields = '__all__'
+
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['course'].queryset = Course.objects.none()
+
+        if 'department' in self.data:
+            try:
+                department_id = int(self.data.get('department'))
+                self.fields['course'].queryset = Course.objects.filter(department_id=department_id).order_by('name')
+            except (ValueError, TypeError):
+                pass  # invalid input from the client; ignore and fallback to empty City queryset
+        elif self.instance.pk:
+            self.fields['course'].queryset = self.instance.department.course_set.order_by('name')
